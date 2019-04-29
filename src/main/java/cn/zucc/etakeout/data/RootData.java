050fd74e6e396511c7e8cd96ce28889af469c2ce
bing.zhub <bing.zhub@gmail.com>
2019-04-29 23:25:26 +0800

    finish consumer/product/list api

---
+++ src/main/java/cn/zucc/etakeout/data/RootData.java
@@ -0,0 +1,16 @@
+package cn.zucc.etakeout.data;
+
+import lombok.Data;
+
+@Data
+public class RootData<T> {
+
+    // 自定义状态码
+    private Integer code;
+
+    // 状态码的详细解释
+    private String message;
+
+    // 具体数据
+    private T data;
+}
