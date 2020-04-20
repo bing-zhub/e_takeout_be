b4e013592780a39a7ee875ed90f7db645172c62d
bing.zhub <bing.zhub@gmail.com>
2020-04-20 16:44:01 +0800

    <store> 记录保存

--- pack.sh
+++ pack.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 mvn clean package -DskipTests=true
+rm -f ~/Desktop/dist/server.jar
 mv ./target/*.jar ~/Desktop/dist/server.jar
+scp ~/Desktop/dist/server.jar root@47.93.56.128:/root/eyao
--- src/main/java/cn/zucc/etakeout/controller/WechatController.java
+++ src/main/java/cn/zucc/etakeout/controller/WechatController.java
@@ -7,6 +7,7 @@ import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
 import me.chanjar.weixin.mp.bean.result.WxMpOAuth2AccessToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,9 +23,12 @@ public class WechatController {
     @Autowired
     private WxMpService wxMpService;
 
+    @Value("${wxhost}")
+    String baseurl;
+
     @GetMapping("/authorize")
     public String authorize(@RequestParam("returnUrl") String returnUrl) {
-        String url = "http://api.carwasher.com.cn/wechat/userInfo";
+        String url = baseurl + "/wechat/userInfo";
         String result =  wxMpService.oauth2buildAuthorizationUrl(url,
                 WxConsts.OAUTH2_SCOPE_USER_INFO,
                 URLEncoder.encode(returnUrl));
